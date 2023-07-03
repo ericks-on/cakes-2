ca5973dff0cee9424210c328a00ca496c11d87f5
Erickson Mbuvic <mutisyaerickson@gmail.comc>
2023-07-04 00:31:08 +0300

    edited the default template

--- web/kimuka.py
+++ web/kimuka.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """This is the route for landing page"""
-    return render_template('index.html')
+    return render_template('default.html', urlFor=url_for)
 
 @app.route('/login', methods=['POST'])
 def login():
