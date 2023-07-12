bef3ac97688126554997044a1d34d8c03bc395b6
Erickson Mbuvic <mutisyaerickson@gmail.comc>
2023-07-13 00:01:32 +0300

    just few changes

--- web/static/scripts/orders.js
+++ web/static/scripts/orders.js
@@ -14,18 +14,5 @@ $(document).ready(function() {
         $('.pdt-analysis-popup').css('display', 'none');
     });
 
-    var statusColumn = $('.order-status-col');
-    for (var i = 0; i < statusColumn.length; i++) {
-        var status = statusColumn[i].innerHTML;
-        if (status == 'pending') {
-            statusColumn[i].addClass('pending');
-        } else if (status == 'confirmed') {
-            statusColumn[i].addClass('confirmed');
-        } else if (status == 'delivered') {
-            statusColumn[i].addClass('delivered');
-        } else if (status == 'settled') {
-            statusColumn[i].addClass('settled');
-        };
-    };
 
 });
