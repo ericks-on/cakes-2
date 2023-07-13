e9d0cc1c1eb4b7ac7fde959a0ea976d2c1fa32df
Erickson Mbuvic <mutisyaerickson@gmail.comc>
2023-07-13 16:49:03 +0300

    finished the product analysis front end section.

--- web/static/scripts/main.js
+++ web/static/scripts/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
     $('#exit-btn').click(function() {
-        $('#popups-container').css('display', 'none');
+        $('#popups-container').hide();
     });
 });
--- web/static/scripts/orders.js
+++ web/static/scripts/orders.js
@@ -4,15 +4,39 @@ $(document).ready(function() {
         $('.orders-popup').css('display', 'block');
     });
 
-    $('#more-pdts-btn').click(function() {
+    $('.product').click(function() {
         $('#popups-container').css('display', 'flex');
-        $('.pdt-analysis-popup').css('display', 'block');
+        var ctx = $('#pa-graph')
+        var lineChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+              labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+              'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+              datasets: [{
+                label: 'Sales',
+                data: [10, 20, 30, 25, 35, 45],
+                borderColor: 'blue',
+                fill: false
+              }]
+            },
+            options: {
+              responsive: true,
+              scales: {
+                y: {
+                  beginAtZero: true
+                }
+              }
+            }
+          });
+        $('.pa-popup').show();
+        $('pa-graph').show();
     });
 
     $('#exit-btn').click(function() {
-        $('.orders-popup').css('display', 'none');
-        $('.pdt-analysis-popup').css('display', 'none');
+        $('#popups-container').hide();
+        $('.pa-popup').hide();
+        $('pa-graph').hide();
+        $('.orders-popup').hide();
     });
-
 
 });
