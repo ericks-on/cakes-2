bf2be8d7519d258bf74158ade54f7f03b909f471
Erickson Mbuvic <mutisyaerickson@gmail.comc>
2023-07-26 18:15:21 +0300

    added new order button

--- web/static/scripts/emoji.js
+++ web/static/scripts/emoji.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-    const inputField = $('#chat-input');
-    new EmojiMart.EmojiPicker({
+    const inputField = $('#chat-emoji-btn');
+    new emojiMart.EmojiPicker({
         autoHide: true, // The picker will automatically hide after selecting an emoji
         showPreview: false, // Don't show the preview of the emoji when hovering
         showSearch: false, // Disable the search bar
