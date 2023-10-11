7a259441fd3e07f8bdca8f7dc01918a804082b17
erickson <mutisyaerickson@gmail.com>
2023-10-11 10:23:02 +0300

    now the action button to launch the cart is functional

--- web_new/static/scripts/buttons.js
+++ web_new/static/scripts/buttons.js
@@ -1,9 +1,17 @@
 $(document).ready(function(){
     var exitPopup = $("#exit-popup");
 
+    // Closing the popups container
     exitPopup.click(function(){
         $("#popups-container").hide();
-        $("#popups-container").find("*").not('#exit-popup').hide();
+        $("#popups-container").find("*").not('#exit-popup, #exit-popup *').hide();
+    });
+
+    // Displaying the cart
+    $('#cart-ab').click(function(){
+        $("#popups-container").css("display", "flex");
+        $('#cart-container').css("display", "flex");
+        $("#cart-container *").show();
     });
 }
 );
