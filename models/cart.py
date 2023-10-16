67dfba0749057a0a2c031932ca84a563b5ca818b
erickson <mutisyaerickson@gmail.com>
2023-10-16 23:05:25 +0300

    added the cart model

---
+++ models/cart.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""The cart"""
+from sqlalchemy import Column, String, Integer, ForeignKey
+from models import Basemodel, Base
+
+
+class cart(Basemodel, Base):
+    """The cart"""
+    __tablename__ = 'cart'
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    product_id = Column(String(60), ForeignKey('products.id'), nullable=False)
+    quantity = Column(Integer, nullable=False, default=1)
