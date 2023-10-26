e466a475477d255f9903c4b40568f2be42150c72
erickson <mutisyaerickson@gmail.com>
2023-10-26 17:44:02 +0300

    login script not required

--- web_new/static/scripts/main.js
+++ web_new/static/scripts/main.js
@@ -1,20 +1,4 @@
 $(document).ready(function(){
-    // login
-    $('#login-button').click(function(event){
-        console.log('login button clicked')
-        event.preventDefault();
-        $.post('/', {
-            username: $('#login-username').val(),
-            password: $('#login-password').val()
-        }, function(data, status){
-            $('#loginForm').submit();
-        }).fail(function(data, status){
-            let errorMessages = JSON.parse(data.responseJSON['error'])['msg'];
-            alert(errorMessages);
-            return;
-        });
-    });
-
     const alertContainer = $('#alertPopup .alertPopupMessage');
     function customAlert(message) {
         alertContainer.text(message);
